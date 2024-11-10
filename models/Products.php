f633ed7d99626c32f3838f32b10672a22cbcffc3
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2024-11-10 22:22:14 +0700

    Add Categories and Products model classes with validation rules and relationships

---
+++ models/Products.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "products".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property float $price
+ * @property int $stock
+ * @property int|null $category_id
+ *
+ * @property Categories $category
+ */
+class Products extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'products';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'price', 'stock'], 'required'],
+            [['description'], 'string'],
+            [['price'], 'number'],
+            [['stock', 'category_id'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Categories::class, 'targetAttribute' => ['category_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'description' => 'Description',
+            'price' => 'Price',
+            'stock' => 'Stock',
+            'category_id' => 'Category ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[Category]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Categories::class, ['id' => 'category_id']);
+    }
+}
